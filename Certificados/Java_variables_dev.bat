d13ce0077e5c0c8743227384a91cfdf642ecfb26
CRITICAL\rmenezes <romarioinf@gmail.com>
2024-03-09 13:48:46 +0000

    added folder certificate

---
+++ Certificados/Java_variables_dev.bat
@@ -0,0 +1,9 @@
+set UNITY_MODE=SIMULATION
+set UNITY_PATH=C:\Users\rmenezes\Documents\application-java-moduwebunity-backend
+
+set UNITY_WEBAPP_PATH=C:\Users\rmenezes\Documents\Trabalho Repository\application-angular-moduwebunity-frontend\dist
+
+set _JAVA_OPTIONS=-Djava.net.preferIPv4Stack=true
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_202
+set JRE_HOME=C:\Program Files\Java\jre1.8.0_202
+set DB_PATH=%UNITY_PATH%\db-data
---
+++ Certificados/Java_variables_test.bat
@@ -0,0 +1,9 @@
+set UNITY_MODE=SIMULATION
+set UNITY_PATH=C:\Users\rmenezes\Documents\workspace-test-backend\backend
+
+set UNITY_WEBAPP_PATH=C:\Users\rmenezes\Documents\workspace-test-frontend\front-end\dist
+
+set _JAVA_OPTIONS=-Djava.net.preferIPv4Stack=true
+set JAVA_HOME=C:\Program Files\Java\jdk-11.0.11
+set JRE_HOME=
+set DB_PATH=%UNITY_PATH%\db-data
